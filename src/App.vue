179e94fdc3668651e6120db28192a5161cee6ba5
MedHarrabi <94125807+MedHarrabi@users.noreply.github.com>
2024-01-07 13:42:03 +0100

    enhanced front part

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,12 @@ body {
   font-family: "Heebo", Avenir, Helvetica, Arial, sans-serif;
   margin: 0;
 }
+
+.wrapper {
+  background-image: url('./assets/background.jpeg'); /* Replace 'path/to/your/image.jpg' with the actual path to your image */
+  background-size: cover; /* Ensures the image covers the entire container */
+  background-position: center; /* Centers the image within the container */
+  background-repeat: no-repeat; /* Prevents the image from repeating */
+  min-height: 100vh;
+}
 </style>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -8,82 +8,178 @@ import SupportIcon from './icons/IconSupport.vue'
 </script>
 
 <template>
-  <h1>WELCOME</h1>
-<!--  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
-
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
-
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
-      >Cypress Component Testing</a
-    >.
-
-    <br />
-
-    More instructions are available in <code>README.md</code>.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>-->
+  <div class="container">
+    <h1 class="text-slider">Book Fast</h1>
+    <div class="form-wrapper">
+      <VaForm ref="myForm" stateful class="mb-2 flex flex-col gap-2">
+        <VaInput class="mr-5 b-dark" name="firstName" label="First Name" />
+        <VaInput class="b-dark" name="lastName" label="Last Name" />
+        <VaDateInput class="mr-5 b-dark" name="birthDay" label="Birth day" />
+        <VaButton
+            type="submit"
+            class="mt-4 ml-5"
+        >
+          Search
+        </VaButton>
+      </VaForm>
+    </div>
+    <div class="cards-wrapper">
+      <VaCard class="travel-card">
+        <VaCardTitle>Travel card</VaCardTitle>
+        <div class="info-wrapper my-3">
+          <h2>Origin</h2>
+          <h3>Destination</h3>
+        </div>
+        <div class="info-wrapper my-3">
+          <h2>Arrival date</h2>
+          <h3>Deaparture date</h3>
+        </div>
+        <VaCardActions align="between">
+          <VaButton>book</VaButton>
+        </VaCardActions>
+      </VaCard>
+      <VaCard class="travel-card">
+        <VaCardTitle>Travel card</VaCardTitle>
+        <div class="info-wrapper my-3">
+          <h2>Origin</h2>
+          <h3>Destination</h3>
+        </div>
+        <div class="info-wrapper my-3">
+          <h2>Arrival date</h2>
+          <h3>Deaparture date</h3>
+        </div>
+        <VaCardActions align="between">
+          <VaButton>book</VaButton>
+        </VaCardActions>
+      </VaCard>
+      <VaCard class="travel-card">
+        <VaCardTitle>Travel card</VaCardTitle>
+        <div class="info-wrapper my-3">
+          <h2>Origin</h2>
+          <h3>Destination</h3>
+        </div>
+        <div class="info-wrapper my-3">
+          <h2>Arrival date</h2>
+          <h3>Deaparture date</h3>
+        </div>
+        <VaCardActions align="between">
+          <VaButton>book</VaButton>
+        </VaCardActions>
+      </VaCard>
+      <VaCard class="travel-card">
+        <VaCardTitle>Travel card</VaCardTitle>
+        <div class="info-wrapper my-3">
+          <h2>Origin</h2>
+          <h3>Destination</h3>
+        </div>
+        <div class="info-wrapper my-3">
+          <h2>Arrival date</h2>
+          <h3>Deaparture date</h3>
+        </div>
+        <VaCardActions align="between">
+          <VaButton>book</VaButton>
+        </VaCardActions>
+      </VaCard>
+      <VaCard class="travel-card">
+        <VaCardTitle>Travel card</VaCardTitle>
+        <div class="info-wrapper my-3">
+          <h2>Origin</h2>
+          <h3>Destination</h3>
+        </div>
+        <div class="info-wrapper my-3">
+          <h2>Arrival date</h2>
+          <h3>Deaparture date</h3>
+        </div>
+        <VaCardActions align="between">
+          <VaButton>book</VaButton>
+        </VaCardActions>
+      </VaCard>
+      <VaCard class="travel-card">
+        <VaCardTitle>Travel card</VaCardTitle>
+        <div class="info-wrapper my-3">
+          <h2>Origin</h2>
+          <h3>Destination</h3>
+        </div>
+        <div class="info-wrapper my-3">
+          <h2>Arrival date</h2>
+          <h3>Deaparture date</h3>
+        </div>
+        <VaCardActions align="between">
+          <VaButton>book</VaButton>
+        </VaCardActions>
+      </VaCard>
+      <VaCard class="travel-card">
+        <VaCardTitle>Travel card</VaCardTitle>
+        <div class="info-wrapper my-3">
+          <h2>Origin</h2>
+          <h3>Destination</h3>
+        </div>
+        <div class="info-wrapper my-3">
+          <h2>Arrival date</h2>
+          <h3>Deaparture date</h3>
+        </div>
+        <VaCardActions align="between">
+          <VaButton>book</VaButton>
+        </VaCardActions>
+      </VaCard>
+      <VaCard class="travel-card">
+        <VaCardTitle>Travel card</VaCardTitle>
+        <div class="info-wrapper my-3">
+          <h2>Origin</h2>
+          <h3>Destination</h3>
+        </div>
+        <div class="info-wrapper my-3">
+          <h2>Arrival date</h2>
+          <h3>Deaparture date</h3>
+        </div>
+        <VaCardActions align="between">
+          <VaButton>book</VaButton>
+        </VaCardActions>
+      </VaCard>
+    </div>
+  </div>
 </template>
+<style>
+.form-wrapper {
+  margin-top: 30px;
+  padding: 20px;
+  max-width: 600px;
+  border-radius: 25px;
+  background-color: white;
+  opacity: 0.9;
+}
+
+.form-wrapper b-dark {
+  border-color: black;
+}
+.container {
+  margin-top: 160px;
+  padding: 130px;
+}
+
+.info-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+.travel-card {
+  padding: 20px;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  max-width: 250px;
+  opacity: 0.9;
+}
+.text-slider {
+  font-size: 32px;
+  font-family: "Heebo", Avenir, Helvetica, Arial, sans-serif;
+  color: white;
+}
+
+.cards-wrapper {
+  display: flex;
+  column-gap: 80px;
+  row-gap: 20px;
+  flex-wrap: wrap;
+}
+</style>
